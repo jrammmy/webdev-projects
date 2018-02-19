01f8191cfd66edec3d00b115faa0524557653cc1
jrammmy <justinramiscal@gmail.com>
2018-02-18 17:20:17 -0800

    Fixed up the todo list, added logic and more css.

--- todoList/scripts/todoList.js
+++ todoList/scripts/todoList.js
@@ -1,42 +1,99 @@
-var editBtn = document.querySelector('#edit');
+var addBtn = document.querySelector('#add');
 var input = document.querySelectorAll('input')[0];
-var date = document.querySelectorAll('input')[1];
+var deleteIcon = document.querySelectorAll('.trash');
+var editText = document.querySelectorAll('.editText');
 
-var deleteBtn = document.querySelectorAll('.test');
-
-editBtn.addEventListener('click', function () {
+addBtn.addEventListener('click', function() {
   input.classList.toggle('hide');
-  date.classList.toggle('hide');
 });
 
-for (var i = 0; i < deleteBtn.length; i++) {
-  deleteBtn[i].addEventListener('click', function () {
-    this.parentElement.style.display = 'none';
+for (var i = 0; i < deleteIcon.length; i++) {
+  deleteIcon[i].addEventListener('click', function() {
+    this.parentElement.remove();
   });
 }
 
-input.addEventListener('keypress', function (e) {
+for (var i = 0; i < editText.length; i++) {
+  editText[i].addEventListener('dblclick', edit);
+}
+
+input.addEventListener('keypress', function(e) {
   var enter = 13;
   if (e.which === enter) {
 
     var li = document.createElement('li');
-    var i = document.createElement('i');
-    var span = document.createElement('span');
-    span.className = 'test';
-    i.className = 'fas fa-trash-alt';
-    span.addEventListener('click', function () {
-      this.parentElement.style.display = 'none';
+    var iTrash = document.createElement('i');
+    var trashSpan = document.createElement('span');
+    var editSpan = document.createElement('span');
+    var input = document.createElement('input');
+    var taskLabel = document.createElement('label');
+    var dateLabel = document.createElement('small');
+
+    var liText = document.createTextNode(this.value); // refers to the input for a new todo
+    var date = document.createTextNode('Posted: ' + getDate());
+    var space = document.createTextNode(" ");
+
+    trashSpan.className = 'trash';
+    editSpan.className = 'editText';
+    iTrash.className = 'fas fa-trash-alt';
+    taskLabel.className = 'task';
+    input.setAttribute('type', 'text');
+    dateLabel.setAttribute('style', 'float: right;');
+
+    trashSpan.addEventListener('click', function() {
+      this.parentElement.remove();
     });
 
-    // refers to the input for a new todo
-    var liText = document.createTextNode(this.value);
-    var liSpace = document.createTextNode(' ');
-    span.appendChild(i);
-    li.appendChild(span);
-    li.appendChild(liSpace);
-    li.appendChild(liText);
+    trashSpan.appendChild(iTrash);
+    li.addEventListener('mouseenter', function() {
+      editSpan.appendChild(trashSpan);
+    });
+
+    li.addEventListener('mouseleave', function() {
+      editSpan.removeChild(trashSpan);
+    });
+
+    editSpan.addEventListener('dblclick', edit);
+
+    li.appendChild(editSpan);
+    taskLabel.appendChild(liText);
+    dateLabel.appendChild(date);
+    editSpan.appendChild(taskLabel);
+    editSpan.appendChild(dateLabel);
+    editSpan.appendChild(input);
     document.querySelector('.todoList').appendChild(li);
     this.value = '';
-
   }
 });
+
+function edit() {
+  var editInput = this.parentElement.querySelector('input[type=text]');
+  var label = this.parentElement.querySelector('label');
+  var containsClass = this.parentElement.classList.contains('editMode');
+  var deleteIcon = document.querySelectorAll('.trash');
+  var date = document.querySelector('small');
+
+  for (var i = 0; i < deleteIcon.length; i++) {
+    deleteIcon[i].classList.toggle('hide');
+  }
+
+  if (containsClass) {
+    label.innerText = editInput.value;
+  } else {
+    editInput.value = label.innerText;
+  }
+
+  date.innerText = 'Updated: ' + getDate();
+  this.parentElement.classList.toggle('editMode');
+}
+
+function getDate() {
+  var now = new Date();
+  var month = now.getMonth() + 1;
+  var day = now.getDate();
+  var year = now.getFullYear();
+
+  var date = + month + "-" + day + "-" + year;
+
+  return date;
+}
